e176a443d4798f420e8e7c14fffb55e5a3e910cb
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-28 20:04:17 -0300

    Feat: Creating the project's 'Home Page'

--- frontend/Ecommerce/src/views/HomeView.vue
+++ frontend/Ecommerce/src/views/HomeView.vue
@@ -3,7 +3,14 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="min-h-screen flex flex-col bg-gray-100">
+    <div class="w-full h-[400px] bg-cover bg-center bg-no-repeat"
+      style="background-image: url('https://img.freepik.com/fotos-gratis/luminaria-de-neon-para-vendas-de-sexta-feira-negra_23-2151833076.jpg?semt=ais_hybrid&w=740');">
+      <div class="h-full flex flex-col justify-center pl-20 text-white bg-black/60">
+        <h1 class="text-6xl font-bold mb-4 tracking-wide">EcommerceProject</h1>
+        <p class="text-2xl font-light italic">Aqui você encontra aquilo que busca.</p>
+      </div>
+    </div>
+  </div>
 </template>
+
