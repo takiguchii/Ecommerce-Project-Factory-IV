4f77df1cf0ced882b40e225bf06238b6f4be3953
Raul <raulfabiano.dev@gmail.com>
2025-09-04 20:43:47 -0300

    feat: add form fields for login and register in loginView

--- frontend/Ecommerce/src/views/LoginView.vue
+++ frontend/Ecommerce/src/views/LoginView.vue
@@ -3,6 +3,32 @@
     <section class="w-full max-w-sm">
       <div class="bg-white shadow rounded-xl p-6">
         <h1 class="text-2xl font-semibold mb-4">Login / Cadastro</h1>
+
+        <form>
+          <div class="mb-3">
+            <label class="block text-sm mb-1">Nome</label>
+            <input type="text" placeholder="Seu nome" class="w-full border rounded px-3 py-2"/>
+          </div>
+
+          <div class="mb-3">
+            <label class="block text-sm mb-1">E-mail</label>
+            <input type="email" placeholder="seu@email.com" class="w-full border rounded px-3 py-2"/>
+          </div>
+
+          <div class="mb-3">
+            <label class="block text-sm mb-1">Senha</label>
+            <input type="password" placeholder="********" class="w-full border rounded px-3 py-2"/>
+          </div>
+
+          <div class="mb-3">
+            <label class="block text-sm mb-1">Confirmar Senha</label>
+            <input type="password" placeholder="********" class="w-full border rounded px-3 py-2"/>
+          </div>
+
+          <button type="submit" class="w-full bg-gray-900 text-white rounded py-2 mt-2">
+            Entrar
+          </button>
+        </form>
       </div>
     </section>
   </main>
@@ -10,8 +36,3 @@
 
 <script setup>
 </script>
-
-<style scoped>
-
-</style>
-
