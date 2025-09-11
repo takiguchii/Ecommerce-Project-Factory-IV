0cbd43e008740cc9551c680a8c65fb3ed59451ee
Takiguchii <alexandre.pupim360@gmail.com>
2025-09-10 21:10:09 -0300

    Feat: Adding  ProductRepository

--- backend/Ecommerce/Repositories/ProductRepository.cs
+++ backend/Ecommerce/Repositories/ProductRepository.cs
@@ -1,6 +1,35 @@
+using Ecommerce.Data;
+using Ecommerce.Entity;
+using Ecommerce.Interfaces; 
+
 namespace Ecommerce.Repositories;
 
-public class ProductRepository
+public class ProductRepository : IProductRepository
 {
-    
+    private readonly EcommerceDbContext _dbContext;
+
+    public ProductRepository(EcommerceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Add(Product product)
+    {
+        _dbContext.Products.Add(product);
+    }
+
+    public List<Product> GetAll()
+    {
+        return _dbContext.Products.ToList();
+    }
+
+    public Product? GetById(int id)
+    {
+        return _dbContext.Products.FirstOrDefault(p => p.Id == id);
+    }
+
+    public void SaveChanges()
+    {
+        _dbContext.SaveChanges();
+    }
 }
