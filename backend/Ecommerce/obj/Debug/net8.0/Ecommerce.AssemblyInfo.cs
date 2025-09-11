268520cbf2a8736549969973738352c5478ebe64
Takiguchii <alexandre.pupim360@gmail.com>
2025-09-10 21:00:55 -0300

    Feat: Create IProductRepository

--- backend/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ backend/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+809ca3a9f5dbcd74f04c234336da553933fa0e8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844a1bbfa66a7126ed43f3fc18c18108ee4a5d4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
