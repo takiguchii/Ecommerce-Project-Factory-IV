ad64d2fbbbc68bd1e7eab6d095a427c56acfb7e3
Takiguchii <alexandre.pupim360@gmail.com>
2025-09-20 13:08:27 -0300

    Feat: Create Auxiliary Entity 'Category'

--- backend/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ backend/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ca4f73611c423f06d0ac5c208ae892b8ecb62b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33acb082df07755b11beaea3d9cfe47223742141")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
