771a9884239769437a072efa9324f08731337ac2
Takiguchii <alexandre.pupim360@gmail.com>
2025-09-20 16:32:37 -0300

    Feat: Creating product mapping for category

--- backend/Ecommerce/Data/Context/EcommerceDbContext.cs
+++ backend/Ecommerce/Data/Context/EcommerceDbContext.cs
@@ -14,7 +14,16 @@ public class EcommerceDbContext : DbContext
     public DbSet<SubCategory> SubCategories { get; set; }
     public DbSet<Provider> Providers { get; set; }
 
-
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Relação do produto com a categoria ( Um para muitos ) 
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Category)
+            .WithMany(c=> c.Products)
+            .HasForeignKey(p => p.CategoryId);
+        
+        base.OnModelCreating(modelBuilder);
+    }
 
 
 
