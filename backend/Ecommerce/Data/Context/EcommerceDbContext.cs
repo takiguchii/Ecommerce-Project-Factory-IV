20f8916e7cbc53498d3793da3e493cd3d5aab9bb
Takiguchii <alexandre.pupim360@gmail.com>
2025-09-20 16:31:49 -0300

    Feat: Declaring all Dbsets from tables

--- backend/Ecommerce/Data/Context/EcommerceDbContext.cs
+++ backend/Ecommerce/Data/Context/EcommerceDbContext.cs
@@ -8,7 +8,15 @@ public class EcommerceDbContext : DbContext
     public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : base(options)
     {
     }
+    
     public DbSet<Product> Products { get; set; } 
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<SubCategory> SubCategories { get; set; }
+    public DbSet<Provider> Providers { get; set; }
+
+
+
+
 
     
 }
