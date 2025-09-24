11d10358678f8a41afa09d6a7aa47be33f2862c8
Takiguchii <alexandre.pupim360@gmail.com>
2025-09-24 10:40:26 -0300

    Feat: Creating ProviderDTOs

--- backend/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ backend/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+999f83db9e5b8b00643ce4aacd8db32d376e173d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b4ea4fbe98988aed90ac2e526678c97d538655")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
