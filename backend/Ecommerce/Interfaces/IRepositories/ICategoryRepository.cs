03c026606cfc96d978a4511024ade3d99f8a841f
Takiguchii <alexandre.pupim360@gmail.com>
2025-09-24 07:13:46 -0300

    Feat: Creating ICategoryRepository

---
+++ backend/Ecommerce/Interfaces/IRepositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+using Ecommerce.Entity;
+
+namespace Ecommerce.Interfaces.Repositories;
+
+public interface ICategoryRepository
+{
+    void Add(Category category);
+    List<Category> GetAll();
+    Category? GetById(int id);
+    void Delete(Category category);
+    void SaveChanges();
+}
