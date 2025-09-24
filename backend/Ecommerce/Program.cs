48b403e035ddc974aa59eead7f0572a9566e9f49
Takiguchii <alexandre.pupim360@gmail.com>
2025-09-24 08:54:32 -0300

    Feat: Creating dependency injection for the subcategory of program.cs

--- backend/Ecommerce/Program.cs
+++ backend/Ecommerce/Program.cs
@@ -21,6 +21,9 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 
+builder.Services.AddScoped<ISubCategoryRepository, SubCategoryRepository>();
+builder.Services.AddScoped<ISubCategoryService, SubCategoryService>();
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
