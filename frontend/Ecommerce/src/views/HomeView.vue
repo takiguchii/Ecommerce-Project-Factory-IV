57df3e9ab6a9a46c9cbe2fe031147ebef361660c
Takiguchii <alexandre.pupim360@gmail.com>
2025-10-02 20:56:13 -0300

    Feat: creating product component

--- frontend/Ecommerce/src/views/HomeView.vue
+++ frontend/Ecommerce/src/views/HomeView.vue
@@ -1,8 +1,5 @@
 <script setup>
-// 1. Importe o novo componente com o nome correto
 import HeroCarouselComponent from '@/components/HeroCarouselComponent.vue';
-
-// A lógica dos produtos virá aqui depois
 </script>
 
 <template>
