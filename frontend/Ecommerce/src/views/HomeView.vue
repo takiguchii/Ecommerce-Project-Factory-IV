a1201719057d7585fee0cb7096ee9a0ee556c7c0
Takiguchii <alexandre.pupim360@gmail.com>
2025-10-04 15:03:33 -0300

    Refactor: modifying file call in homeview

--- frontend/Ecommerce/src/views/HomeView.vue
+++ frontend/Ecommerce/src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <script setup>
 import HeroCarouselComponent from '@/components/HeroCarouselComponent.vue';
 import ProductSectionComponent from '@/components/ProductSectionComponent.vue';
+
+import { usePromotions } from '@/composables/usePromotions.js';
+
+const { promotions, loading: promotionsLoading, error: promotionsError, fetchPromotions } = usePromotions();
 </script>
 
 <template>
   <div>
     <HeroCarouselComponent />
-    <ProductSectionComponent class="text-center" title="Nossos Produtos" />
+
+    <ProductSectionComponent title="Nossos Produtos" />
+
+    <ProductSectionComponent 
+      title="Hora de Dar Play!"
+      :products="promotions"
+      :loading="promotionsLoading"
+      :error="promotionsError"
+      @fetch-needed="fetchPromotions"
+    />
+    
     </div>
 </template>
