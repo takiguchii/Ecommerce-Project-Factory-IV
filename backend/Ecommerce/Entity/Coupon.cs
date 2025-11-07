31b07eac9d2b70ec2a8740780f781787f15580f1
Takiguchii <alexandre.pupim360@gmail.com>
2025-11-07 20:32:25 -0300

    Feat: Creating CouponEntity

---
+++ backend/Ecommerce/Entity/Coupon.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Entity
+{
+    public class Coupon
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Code { get; set; } 
+
+        [Required]
+        public decimal DiscountValue { get; set; } 
+
+        [Required]
+        public bool IsPercentage { get; set; } 
+
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
