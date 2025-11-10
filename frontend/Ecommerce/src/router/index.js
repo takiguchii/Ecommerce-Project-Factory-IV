1d6d14dc7ed8122e516f78eb116218b01c7b7493
Raul <raulfabiano.dev@gmail.com>
2025-11-10 20:53:15 -0300

    feat: Create router Admin coupon page

--- frontend/Ecommerce/src/router/index.js
+++ frontend/Ecommerce/src/router/index.js
@@ -14,6 +14,7 @@ import AdminProviderView from '../views/AdminProviderView.vue';
 import ProfileView from '../views/ProfileView.vue';
 import { jwtDecode } from 'jwt-decode';
 import CartView from '@/views/CartView.vue';
+import AdminCouponView from '@/views/AdminCouponView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -107,6 +108,11 @@ const router = createRouter({
           path: 'providers',
           name: 'admin-providers',
           component: AdminProviderView
+        },
+        {
+          path: 'Coupon',
+          name: 'admin-coupons',
+          component: AdminCouponView
         }
       ]
     }
